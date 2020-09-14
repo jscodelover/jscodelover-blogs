4df861ede908d042f6c0588cac88980b2f4a2bb7
jscodelover <manisha.4918@gmail.com>
2020-09-14 20:19:31 +0530

    build: add some config plugin- style-component, robots-txt, manifest, offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,30 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Manisha Basra",
+    description:
+      "Manisha Basra is a software engineer, based in India, specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.",
+    siteUrl: "https://jscodelover.netlify.com", // No trailing slash allowed!
+    image: "/og.png", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@jscodelover",
+  },
+
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-robots-txt",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "ManishaBasra",
+        short_name: "ManishaBasra",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        display: "minimal-ui",
+        icon: "src/images/logo.png",
+      },
+    },
+    "gatsby-plugin-offline",
+  ],
 }
